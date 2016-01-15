fe3b956cb91711b2377c53bb12d587a935322893
nicheprogramming <captainless2@gmail.com>
2016-01-15 16:58:23 +0000

    seperated game code from functions.php and began work on initializing games

---
+++ gameLogic.php
@@ -0,0 +1,29 @@
+<?php
+    require('functions.php');
+    
+    function rollDice() {
+        $die1 = rand(1,6);
+        $die2 = rand(1,6);
+        $die3 = rand(1,6);
+        $die4 = rand(1,6);
+        $die5 = rand(1,6);
+        $roll = $die1 + $die2 + $die3 + $die4 + $die5;
+        return $roll;
+    }
+    
+    function turn($player, $playerScore) {
+        $roll = rollDice();
+        $mod = $roll % 2;
+        
+        if($roll >= 20 && $mod == 0) {
+    		$playerScore--;
+    	} else if($roll >= 20 && $mod != 0) {
+    		$playerScore++;
+    	} else if($roll <= 19 && $mod == 0) {
+    		$playerScore++;
+    	} else if($roll <= 19 && $mod != 0) {
+    		$playerScore--;
+    	}
+        echo $player . " rolled " . $roll . ", changing his score to " . $playerScore . ".";
+    }
+?>
